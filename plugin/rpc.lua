1e25beb86b8cc7707bbe3284e281054e3b9e1dbb
Zeke Maroclo-Kemmerling <zkemmerling@gmail.com>
2024-03-24 04:36:20 -0400

    FIX: change command declaration for SpotifyLike to actually connect to API

--- plugin/rpc.lua
+++ plugin/rpc.lua
@@ -1,48 +1,38 @@
-local plugin_directory = vim.fn.fnamemodify(vim.fn.resolve(vim.fn.expand("<sfile>:p")), ":h:h")
+-- local plugin_directory = vim.fn.fnamemodify(vim.fn.resolve(vim.fn.expand("<sfile>:p")), ":h:h")
+local spot = {}
 
-local remote_job_id = nil
-
-local function log(msg)
+spot.log = function(msg)
 	vim.api.nvim_out_write(msg .. "\n")
 end
 
-local function start_plugin()
-	local bin = plugin_directory .. "/target/debug/spotify-vim"
-	if not vim.fn.executable(bin) then
-		log("The nvim-spotify binary is not available.")
+local binary_path = vim.fn.fnamemodify(vim.api.nvim_get_runtime_file("plugin/rpc.lua", false)[1], ":h:h")
+	.. "target/debug/spotify-vim"
+
+spot.start = function()
+	if not vim.fn.executable(binary_path) then
+		spot.log("The nvim-spotify binary is not available.")
+		return
+	end
+	print("executable found")
+	spot.id = vim.fn.jobstart({ binary_path }, { rpc = true })
+	if spot.id <= 0 then
+		spot.log("Failed to spawn spotify plugin")
 		return
 	end
 
-	local id = vim.fn.jobstart(bin, { rpc = true })
-	if id <= 0 then
-		log("Failed to spawn spotify plugin")
-		return
-	end
-	return id
+	print("New job...", spot.job_id)
+	spot.cmds()
 end
 
-local function like_song()
-	vim.rpcnotify(remote_job_id, "like")
-	vim.notify_once("rpc request to spotify : like song")
+spot.notify = function(method, ...)
+	spot.start()
+	vim.rpcnotify(spot.id, method, ...)
+	print("Sending rpc notify ...", method)
 end
 
-local function define_commands()
-	vim.cmd([[command! SpotifyLike lua like_song() ]])
+spot.cmds = function()
+	vim.cmd([[command! SpotifyLike lua require'spot'.notify('like') ]])
 	vim.cmd([[command! SpfyDb lua vim.notify('Test!')]])
 end
 
-local function setup()
-	-- vim.opt.completeopt = { "menuone", "noinsert", "noselect" }
-	-- vim.opt.shortmess = vim.opt.shortmess + "c"
-
-	remote_job_id = start_plugin()
-	if remote_job_id ~= nil then
-		-- no autocmds or refreshing? we just need to fire off a rpc request and call it a day
-		define_commands()
-	end
-end
-
-return {
-	setup = setup,
-	like_song = like_song,
-}
+return spot
